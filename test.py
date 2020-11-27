bb97f9d12d921c080aaec4d3fa66d1204c8e0589
Drakodav <vmedves@gmail.com>
2020-11-27 14:46:53 +0000

    feat✅: working yup

--- test.py
+++ test.py
@@ -1,28 +1,75 @@
+import cv2
+import sys
+import numpy as np
 
 
-def combineBoundingBox(box1, box2):
-    x = box1[0] if box1[0] < box2[0] else box2[0]
-    y = box1[1] if box1[1] < box2[1] else box2[1]
-    w = box1[2] if box1[2] > box2[2] else box2[2]
-    h = box1[3] if box1[3] > box2[3] else box2[3]
-
-    return (x, y, w, h)
-
-def touchingRect(box1, box2):
-    if box1[0] < box2[0] + box2[2] and \
-    box1[0] + box1[2] > box2[0] and \
-    box1[1] < box2[1] + box2[3] and \
-    box1[1] + box1[3] > box2[1]:
-        return True
-    else:
-        return False
+def nothing(x):
+    pass
 
 
-box1 = (60, 20, 60, 60)
-box2 = (10, 10, 500, 50)
+# Load in image
+image = cv2.imread('./images/pos_641.jpg')
 
-print(touchingRect(box1, box2))
+# Create a window
+cv2.namedWindow('image')
 
-x, y, w, h = combineBoundingBox(box1,box2)
+# create trackbars for color change
+# Hue is from 0-179 for Opencv
+cv2.createTrackbar('HMin', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
 
-print(x, y, w, h)
+# Set default value for MAX HSV trackbars.
+cv2.setTrackbarPos('HMax', 'image', 179)
+cv2.setTrackbarPos('SMax', 'image', 255)
+cv2.setTrackbarPos('VMax', 'image', 255)
+
+# Initialize to check if HSV min/max value changes
+hMin = sMin = vMin = hMax = sMax = vMax = 0
+phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+output = image
+wait_time = 33
+
+while(1):
+
+    # get current positions of all trackbars
+    hMin = cv2.getTrackbarPos('HMin', 'image')
+    sMin = cv2.getTrackbarPos('SMin', 'image')
+    vMin = cv2.getTrackbarPos('VMin', 'image')
+
+    hMax = cv2.getTrackbarPos('HMax', 'image')
+    sMax = cv2.getTrackbarPos('SMax', 'image')
+    vMax = cv2.getTrackbarPos('VMax', 'image')
+
+    # Set minimum and max HSV values to display
+    lower = np.array([hMin, sMin, vMin])
+    upper = np.array([hMax, sMax, vMax])
+
+    # Create HSV Image and threshold into a range.
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    # Print if there is a change in HSV value
+    if((phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax)):
+        print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (
+            hMin, sMin, vMin, hMax, sMax, vMax))
+        phMin = hMin
+        psMin = sMin
+        pvMin = vMin
+        phMax = hMax
+        psMax = sMax
+        pvMax = vMax
+
+    # Display output image
+    cv2.imshow('image', output)
+
+    # Wait longer to prevent freeze for videos.
+    if cv2.waitKey(wait_time) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
